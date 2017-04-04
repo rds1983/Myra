a56d4072452c490c4463941505ab7ca67ea940c9
Roman Shapiro <eleroman@mail.ru>
2017-04-04 23:17:16 +0700

    Moved source files into the dedicated folder

---
+++ Source/Myra/Utility/Reflection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+
+namespace Myra.Utility
+{
+	public static class Reflection
+	{
+		public static T FindAttribute<T>(this MemberInfo property) where T : Attribute
+		{
+			var result = (from T a in property.GetCustomAttributes<T>(true) select a).FirstOrDefault();
+
+			return result;
+		}
+
+		public static T FindAttribute<T>(this Type type) where T : Attribute
+		{
+			var result = (from T a in type.GetTypeInfo().GetCustomAttributes<T>(true) select a).FirstOrDefault();
+
+			return result;
+		}
+	}
+}
