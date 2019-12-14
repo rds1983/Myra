657af364837b433a1d2c96d24db11c3c59c8e2d1
Roman Shapiro <eleroman@mail.ru>
2019-12-14 15:07:34 +0700

    Added DebugConsole sample

---
+++ samples/Myra.Samples.DebugConsole/DebugConsoleGame.cs
@@ -0,0 +1,66 @@
+﻿using Myra.Graphics2D.UI;
+using Microsoft.Xna.Framework;
+
+namespace Myra.Samples.DebugConsole
+{
+	public class DebugConsoleGame : Game
+	{
+		private readonly GraphicsDeviceManager _graphics;
+		private GamePanel _gamePanel;
+		
+		public static DebugConsoleGame Instance { get; private set; }
+
+		public DebugConsoleGame()
+		{
+			Instance = this;
+
+			_graphics = new GraphicsDeviceManager(this)
+			{
+				PreferredBackBufferWidth = 1200,
+				PreferredBackBufferHeight = 800
+			};
+			Window.AllowUserResizing = true;
+			IsMouseVisible = true;
+		}
+
+		protected override void LoadContent()
+		{
+			base.LoadContent();
+
+			MyraEnvironment.Game = this;
+
+			_gamePanel = new GamePanel();
+
+			Desktop.Widgets.Add(_gamePanel);
+
+#if MONOGAME
+			// Inform Myra that external text input is available
+			// So it stops translating Keys to chars
+			Desktop.HasExternalTextInput = true;
+
+			// Provide that text input
+			Window.TextInput += (s, a) =>
+			{
+				Desktop.OnChar(a.Character);
+			};
+#endif
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			base.Draw(gameTime);
+
+			GraphicsDevice.Clear(Color.Black);
+
+			if (_graphics.PreferredBackBufferWidth != Window.ClientBounds.Width ||
+			    _graphics.PreferredBackBufferHeight != Window.ClientBounds.Height)
+			{
+				_graphics.PreferredBackBufferWidth = Window.ClientBounds.Width;
+				_graphics.PreferredBackBufferHeight = Window.ClientBounds.Height;
+				_graphics.ApplyChanges();
+			}
+
+			Desktop.Render();
+		}
+	}
+}
---
+++ samples/Myra.Samples.DebugConsole/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Myra.Samples.DebugConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var game = new DebugConsoleGame())
+				game.Run();
+		}
+	}
+}
