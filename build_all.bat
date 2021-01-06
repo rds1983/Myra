9b57e10745d365fb52443a3f28bd917983d04360
Roman Shapiro <roman.shapiro@grapecity.com>
2021-01-06 13:05:11 +0700

    #280: In Progress

--- build_all.bat
+++
@@ -1,4 +0,0 @@
-dotnet --version
-dotnet build build\Myra.Monogame.sln /p:Configuration=Release --no-incremental
-dotnet build build\Myra.Stride.sln /p:Configuration=Release --no-incremental
-dotnet build build\Myra.PlatformAgnostic.sln /p:Configuration=Release --no-incremental
