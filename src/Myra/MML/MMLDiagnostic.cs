cb3b3df034b892d22147b55bda3ebc47169e1618
TechPizza <piatkowski2002@gmail.com>
2021-02-20 17:01:03 +0100

    Added simple markup diagnostics

---
+++ src/Myra/MML/MMLDiagnostic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace Myra.MML
+{
+    public delegate void MMLDiagnosticAction(MMLDiagnostic diagnostic);
+
+    public class MMLDiagnostic
+    {
+        public List<XObject> TargetElements { get; } = new List<XObject>();
+        public MMLDiagnosticSeverity Severity { get; }
+        public string Group { get; }
+        public string ID { get; }
+        public string Message { get; }
+        
+        public MMLDiagnostic(MMLDiagnosticSeverity severity, string group, string id, string message)
+        {
+            Severity = severity;
+            Group = group;
+            ID = id;
+            Message = message;
+        }
+    }
+}
