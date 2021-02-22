518ac3aa4a7452732f6d7caf1cc303eba45c2ef6
Roman Shapiro <roman.shapiro@grapecity.com>
2021-02-23 03:55:11 +0700

    Fixed #298

---
+++ src/Myra.Tests/StackPanelTests.cs
@@ -0,0 +1,28 @@
+﻿using Myra.Graphics2D.UI;
+using NUnit.Framework;
+
+namespace Myra.Tests
+{
+	[TestFixture]
+	public class StackPanelTests: BaseTests
+	{
+		[Test]
+		public void AddRemove()
+		{
+			var stackPanel = new VerticalStackPanel();
+			var label1 = new Label();
+			var label2 = new Label();
+			var label3 = new Label();
+
+			stackPanel.AddChild(label1);
+			stackPanel.AddChild(label2);
+			stackPanel.AddChild(label3);
+
+			stackPanel.RemoveChild(label2);
+
+			Assert.AreEqual(2, stackPanel.Widgets.Count);
+			Assert.AreEqual(label1, stackPanel.Widgets[0]);
+			Assert.AreEqual(label3, stackPanel.Widgets[1]);
+		}
+	}
+}
