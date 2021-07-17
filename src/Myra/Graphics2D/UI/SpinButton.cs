75ff99b65781973f8e484a39be0059e39fa6c6fc
Roman Shapiro <roman.shapiro@grapecity.com>
2021-07-17 19:36:35 +0700

    Yet another small fix

--- src/Myra/Graphics2D/UI/SpinButton.cs
+++ src/Myra/Graphics2D/UI/SpinButton.cs
@@ -228,7 +228,7 @@ namespace Myra.Graphics2D.UI
 		[Browsable(false)]
 		public TextBox TextBox => _textField;
 
-		protected internal override MouseWheelFocusType MouseWheelFocusType => MouseWheelFocusType.Hover;
+		protected internal override MouseWheelFocusType MouseWheelFocusType => Value != null ? MouseWheelFocusType.Hover : MouseWheelFocusType.None;
 
 		/// <summary>
 		/// Fires when the value is about to be changed
