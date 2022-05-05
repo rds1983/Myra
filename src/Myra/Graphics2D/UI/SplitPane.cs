c78cdb2caf102efef97935d3afffca5dc5f6f2db
Roman Shapiro <netframeworker@gmail.com>
2022-05-05 22:54:47 +0700

    Small fixes

--- src/Myra/Graphics2D/UI/SplitPane.cs
+++ src/Myra/Graphics2D/UI/SplitPane.cs
@@ -153,9 +153,11 @@ namespace Myra.Graphics2D.UI
 			else
 			{
 				_handleDown = (ImageButton)sender;
+
+				var handleGlobalPos = _handleDown.ToGlobal(_handleDown.Bounds.Location);
 				_mouseCoord = Orientation == Orientation.Horizontal
-					? Desktop.TouchPosition.X - _handleDown.Bounds.X
-					: Desktop.TouchPosition.Y - _handleDown.Bounds.Y;
+					? Desktop.TouchPosition.X - handleGlobalPos.X
+					: Desktop.TouchPosition.Y - handleGlobalPos.Y;
 			}
 		}
 
