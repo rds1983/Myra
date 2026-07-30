e27a624cc2496814c12ce2fc063b5ef08320425d
Roman Shapiro <eleroman@mail.ru>
2026-07-30 07:49:34 +0700

    build fix

--- samples/Myra.Samples.PlatformAgnostic.MonoGame/Platform/MGPlatform.cs
+++ samples/Myra.Samples.PlatformAgnostic.MonoGame/Platform/MGPlatform.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using info.lundin.math;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Myra.Graphics2D.UI;
